e8194b23f7f43d606a801863cace98a139ad8254
Pravin Nichal <nichal.pravin2001@gmail.com>
2023-04-10 18:02:02 +0530

    first commit

---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import axios from "axios";
+import "./FileUpload.css";
+const FileUpload = ({ contract, account, provider }) => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("No image selected");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+        const JWT =
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJjZTNhN2QzNy03MjZhLTRmOTgtYTE5OC1jMzc3MzhjZGZhYjciLCJlbWFpbCI6Im5pY2hhbC5wcmF2aW4yMDAxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI0MzQ1NzBkMmRlYzIzYjM2YWY3YSIsInNjb3BlZEtleVNlY3JldCI6IjVjMjkyNDhhM2VjNTBiN2Y0M2NmYWQ0NjMzMDNjNDQwZmJlYzc4OTE2MzNiNzAyMGEzNTI3OTRiOTk4MWFjNmYiLCJpYXQiOjE2Nzg5ODE0NTR9.AXJFCMaGJpF38ms156VqKCQH1yFW3qXXwCzTK5oTM3Y";
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            // pinata_api_key: `b1dc2f765724944a5c75`,
+            // pinata_secret_api_key: `57b80ea1b5743d7fa9e31d81cf1ec9d19e00191384354935cad5e73161e99efd`,
+            "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+            Authorization: JWT,
+          },
+        });
+        const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+        //const signer = contract.connect(provider.getSigner());
+        const signer = contract.connect(provider.getSigner());
+        signer.add(account, ImgHash);
+      } catch (e) {
+        console.log(e);
+        alert("Unable to upload image to Pinata");
+      }
+    }
+    alert("Successfully Image Uploaded");
+    setFileName("No image selected");
+    setFile(null);
+  };
+  const retrieveFile = (e) => {
+    const data = e.target.files[0]; //files array of files object
+    // console.log(data);
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      setFile(e.target.files[0]);
+    };
+    setFileName(e.target.files[0].name);
+    e.preventDefault();
+  };
+  return (
+    <div className="top">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        />
+        <span className="textArea">Image: {fileName}</span>
+        <button type="submit" className="upload" disabled={!file}>
+          Upload File
+        </button>
+      </form>
+    </div>
+  );
+};
+export default FileUpload;
+
+// import { useState } from "react";
+// import axios from "axios";
+// import "./FileUpload.css";
+// function FileUpload({ contract, provider, account }) {
+//   // const [urlArr, setUrlArr] = useState([]);
+//   const [file, setFile] = useState(null);
+//   const [fileName, setFileName] = useState("No image selected");
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       if (file) {
+//         try {
+//           const formData = new FormData();
+//           formData.append("file", file);
+
+//           const resFile = await axios({
+//             method: "post",
+//             url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+//             data: formData,
+//             headers: {
+//               pinata_api_key: `95f328a012f1634eab8b`,
+//               pinata_secret_api_key: `8ea64e6b39c91631c66128a7c0e0dde35a6fbdf797a8393cc5ba8bf8d58e9b54`,
+//               "Content-Type": "multipart/form-data",
+//             },
+//           });
+
+//           const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+//           const signer = contract.connect(provider.getSigner());
+//           signer.add(account, ImgHash);
+
+//           //setUrlArr((prev) => [...prev, ImgHash]);
+
+//           //Take a look at your Pinata Pinned section, you will see a new file added to you list.
+//         } catch (error) {
+//           alert("Error sending File to IPFS");
+//           console.log(error);
+//         }
+//       }
+
+//       alert("Successfully Uploaded");
+//       setFileName("No image selected");
+//       setFile(null); //to again disable the upload button after upload
+//     } catch (error) {
+//       console.log(error.message); //this mostly occurse when net is not working
+//     }
+//   };
+//   const retrieveFile = (e) => {
+//     const data = e.target.files[0];
+//     console.log(data);
+
+//     const reader = new window.FileReader();
+
+//     reader.readAsArrayBuffer(data);
+//     reader.onloadend = () => {
+//       setFile(e.target.files[0]);
+//     };
+//     setFileName(e.target.files[0].name);
+//     e.preventDefault();
+//   };
+//   return (
+//     <div className="top">
+//       <form className="form" onSubmit={handleSubmit}>
+//         <label htmlFor="file-upload" className="choose">
+//           {/*turn around for avoding choose file */}
+//           Choose Image
+//         </label>
+//         <input
+//           disabled={!account} //disabling button when metamask account is not connected
+//           type="file"
+//           id="file-upload"
+//           name="data"
+//           onChange={retrieveFile}
+//         />
+//         <span className="textArea">Image: {fileName}</span>
+//         {/* choose file */}
+//         <button type="submit" disabled={!file} className="upload">
+//           Upload file
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default FileUpload;
